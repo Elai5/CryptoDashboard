d9cc0cab45a4f9d0b12d3a2e7927314b7b5ac442
Elai5 <yvettelaine007@gmail.com>
2025-06-04 16:57:20 +0300

    start of crypto dashboard

---
+++ vite-project./src/App.jsx
@@ -0,0 +1,17 @@
+/** @format */
+
+import React from "react";
+import { Text } from "@chakra-ui/react";
+
+const App = () => {
+  return (
+    <div>
+      {" "}
+      <Text fontSize="sm" color={"red.500"}>
+        In love with React & Next
+      </Text>
+    </div>
+  );
+};
+
+export default App;
