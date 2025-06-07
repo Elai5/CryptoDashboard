538b7feddbc1655a5a4491842fafdc6eaff28b9b
Elai5 <yvettelaine007@gmail.com>
2025-06-07 14:54:48 +0300

    hardcoded total invested and used JS to calculate profit and current value based on the invested porfolio

--- vite-project./src/components/Home.jsx
+++ vite-project./src/components/Home.jsx
@@ -1,8 +1,51 @@
 /** @format */
 
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { CoinContext } from "../context/CoinContext";
 
 const Home = () => {
+  const { allCoin, currency } = useContext(CoinContext);
+  const [displayCoin, setDisplayCoin] = useState([]);
+
+  useEffect(() => {
+    setDisplayCoin(allCoin);
+  }, [allCoin]);
+
+  const totalInvested = 2500;
+  const userPortfolio = [
+    {
+      id: "bitcoin",
+      amount: 0.01,
+      avg_buy_price: 60000,
+    },
+    {
+      id: "ethereum",
+      amount: 0.3,
+      avg_buy_price: 2000,
+    },
+    {
+      id: "solana",
+      amount: 5,
+      avg_buy_price: 100,
+    },
+  ];
+  const currentValue = userPortfolio.reduce((sum, coin) => {
+    const liveCoin = allCoin.find((c) => c.id === coin.id);
+    if (!liveCoin) return sum;
+    return sum + coin.amount * liveCoin.current_price;
+  }, 0);
+
+  const todayChange = userPortfolio.reduce((sum, coin) => {
+    const liveCoin = allCoin.find((c) => c.id === coin.id);
+    if (!liveCoin) return sum;
+    const coinValue = coin.amount * liveCoin.current_price;
+    const change = coinValue * (liveCoin.price_change_percentage_24h / 100);
+    return sum + change;
+  }, 0);
+
+  const profitLoss = currentValue - totalInvested;
+  const gainPercent = (profitLoss / totalInvested) * 100;
+
   return (
     <div className="w-full">
       <div className="w-full flex flex-col px-6 py-10">
@@ -11,17 +54,54 @@ const Home = () => {
         <div className="w-full grid grid-cols-[2fr_2fr_2fr] px-2 py-2 gap-2">
           <div className="flex flex-col bg-blue-300 border-1 border-gray-700 rounded-md px-5 py-3">
             <p>Total Invested</p>
-            <span className="bold text-2xl">$ 2500</span>
+            <span className="bold text-2xl">
+              $
+              {totalInvested.toLocaleString(undefined, {
+                minimumFractionDigits: 2,
+              })}
+            </span>
             <small>Across all assets</small>
           </div>
           <div className="flex flex-col bg-blue-300 border-1 border-gray-700 rounded-md px-5 py-3">
             <p>Current Value</p>
-            <span className="bold text-2xl">$ 2500</span>
+            <span className="bold text-2xl">
+              $
+              {currentValue.toLocaleString(undefined, {
+                minimumFractionDigits: 2,
+              })}
+            </span>
+            <p
+              className="text-xs"
+              style={{ color: gainPercent >= 0 ? "green" : "red" }}
+            >
+              {gainPercent >= 0 ? "+" : ""}
+              {gainPercent.toFixed(2)}%
+            </p>
+
+            {/* <p className="text-xs">+11.90%</p> */}
             <small>All-time portfolio gain</small>
           </div>
           <div className="flex flex-col bg-blue-300 border-1 border-gray-700 rounded-md px-5 py-3">
-            <p>Total Invested</p>
-            <span className="bold text-2xl">$ 2500</span>
+            <p>Today's Profit/Loss</p>
+            {/* <span className="bold text-2xl">$ 1,234.56</span> */}
+            <span
+              className="bold text-2xl"
+              style={{ color: todayChange >= 0 ? "green" : "red" }}
+            >
+              $
+              {todayChange.toLocaleString(undefined, {
+                minimumFractionDigits: 2,
+              })}
+            </span>
+            <p
+              className="text-xs"
+              style={{ color: todayChange >= 0 ? "green" : "red" }}
+            >
+              {todayChange >= 0 ? "+" : ""}
+              {((todayChange / totalInvested) * 100).toFixed(2)}%
+            </p>
+            {/* <p className="text-xs">+2.47%</p> */}
+
             <small>Since last 24 hours</small>
           </div>
         </div>
@@ -55,6 +135,30 @@ const Home = () => {
               <p className="text-sm">Market Cap</p>
               <p className="text-sm">24hr Change</p>
             </div>
+            {displayCoin.slice(0, 10).map((item, index) => (
+              <div
+                className="w-full grid grid-cols-[70px_2fr_1fr_1fr_1fr] gap-4 py-1 border-1 border-gray-500"
+                key={index}
+              >
+                <p>{item.market_cap_rank}</p>
+                <div>
+                  {" "}
+                  <img src={item.image} alt="" className="size-8" />
+                  <p>{item.name + "_" + item.symbol}</p>
+                </div>
+
+                <p>
+                  {currency.symbol} {item.current_price.toLocaleString()}
+                </p>
+                <p>
+                  {Math.floor(item.price_change_percentage_24h * 100) / 100}
+                </p>
+                <p className="">
+                  {currency.symbol}
+                  {item.market_cap.toLocaleString()}
+                </p>
+              </div>
+            ))}
           </div>
         </div>
       </div>
