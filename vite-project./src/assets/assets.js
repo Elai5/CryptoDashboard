b8f359b836408a1be653556cb7fd68b592ab1f70
Elai5 <yvettelaine007@gmail.com>
2025-06-09 14:10:26 +0300

    removed unused images and logo

--- vite-project./src/assets/assets.js
+++ vite-project./src/assets/assets.js
@@ -1,20 +1,9 @@
 /** @format */
-import logo from "./logo.jpg";
-import logo3 from "./logo3.png";
-import logo1 from "./logo1.png";
+
 import logo4 from "./logo4.png";
-import avator from "./avator.jpeg";
-import green from "./greenl.png";
-import man from "./man.jpeg";
 import man2 from "./midjourney.jpeg";
 
 export const assets = {
-  logo,
-  avator,
-  logo3,
-  logo1,
-  green,
   logo4,
-  man,
   man2,
 };
